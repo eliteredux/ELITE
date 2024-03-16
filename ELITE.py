8741299b83e60d1c5954d03ff69a848b7596de10
eliteredux <163480874+eliteredux@users.noreply.github.com>
2024-03-16 07:06:44 +0500

    Update ELITE.py

--- ELITE.py
+++ ELITE.py
@@ -1,1 +1,1 @@
-import elite
+print("We are currently updating our tools. Please be patient as we work on the improvements. We will be back online shortly. Thank you for your understanding and continued support!")
